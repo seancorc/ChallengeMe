f7d6b28b292f1f76cc1aa3c4689662588037f3ac
jwc297@cornell.edu <jwc297@cornell.edu>
2018-11-21 10:33:48 -0800

    Added challenges to ChallengeAPI

---
+++ ChallengeMe/Layouts/ChallengeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChallengeViewController.swift
+//  ChallengeMe
+//
+//  Created by John Smith on 11/21/18.
+//  Copyright © 2018 ChallengeMe. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
